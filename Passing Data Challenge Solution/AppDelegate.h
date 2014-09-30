00498c15aeadbf2d9b1a08411498d7029bd6dbcb
BrainStarr <brian.r.starr@gmail.com>
2014-09-30 15:21:11 -0700

    Initial Commit

---
+++ Passing Data Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Passing Data Challenge Solution
+//
+//  Created by Brian Starr on 9/30/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
